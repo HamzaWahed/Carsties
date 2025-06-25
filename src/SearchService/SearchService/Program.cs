8acfbd007ef521f8820c18fec7087fa81fc7009b
hwahed <hzd454@gmail.com>
2025-06-25 16:33:58 -0300

    add message retry for auction deleted

--- src/SearchService/SearchService/Program.cs
+++ src/SearchService/SearchService/Program.cs
@@ -35,6 +35,13 @@ builder.Services.AddMassTransit(x =>
             e.UseMessageRetry(r => r.Interval(5, 5));
             e.ConfigureConsumer<AuctionCreatedConsumer>(context);
         });
+        
+        cfg.ReceiveEndpoint("search-auction-deleted", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionDeletedConsumer>(context);
+        });
+        
         cfg.ConfigureEndpoints(context);
     });
 });
